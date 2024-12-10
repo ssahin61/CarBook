8965025c89537be7bb8ca97faa88f723c14e364b
ssahin61 <umitsahinerdogan@gmail.com>
2024-12-10 21:23:07 +0300

    added builder.services in controller

---
+++ Infrastructure/CarBook.Persistence/Migrations/20241210181942_ChangeIamgeUrlToImageUrlForAbouts.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarBook.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeIamgeUrlToImageUrlForAbouts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IamgeUrl",
+                table: "About",
+                newName: "ImageUrl");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "About",
+                newName: "IamgeUrl");
+        }
+    }
+}
