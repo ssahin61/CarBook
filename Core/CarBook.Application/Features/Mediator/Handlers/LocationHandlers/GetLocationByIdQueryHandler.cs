b12d2388ae2fd5ae719c649465026228ae7d023c
ssahin61 <umitsahinerdogan@gmail.com>
2024-12-28 15:54:01 +0300

    added mediator for location

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/LocationHandlers/GetLocationByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using CarBook.Application.Features.Mediator.Queries.LocationQueries;
+using CarBook.Application.Features.Mediator.Results.LocationResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.LocationHandlers
+{
+	public class GetLocationByIdQueryHandler : IRequestHandler<GetLocationByIdQuery, GetLocationByIdQueryResult>
+	{
+		private readonly IRepository<Location> _repository;
+
+		public GetLocationByIdQueryHandler(IRepository<Location> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<GetLocationByIdQueryResult> Handle(GetLocationByIdQuery request, CancellationToken cancellationToken)
+		{
+			var values = await _repository.GetByIdAsync(request.Id);
+
+			return new GetLocationByIdQueryResult
+			{
+				LocationId = values.LocationId,
+				Name = values.Name
+			};
+		}
+	}
+}
