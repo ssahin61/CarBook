4e9294ab7af62f124b1c9047792d40403c3dc578
ssahin61 <umitsahinerdogan@gmail.com>
2025-01-01 14:23:32 +0300

    added mediator for social media

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/SocialMediaHandlers/GetSocialMediaByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Application.Features.Mediator.Queries.SocialMediaQueries;
+using CarBook.Application.Features.Mediator.Results.SocialMediaResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.SocialMediaHandlers
+{
+	public class GetSocialMediaByIdQueryHandler : IRequestHandler<GetSocialMediaByIdQuery, GetSocialMediaByIdQueryResult>
+	{
+		private readonly IRepository<SocialMedia> _repository;
+
+		public GetSocialMediaByIdQueryHandler(IRepository<SocialMedia> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<GetSocialMediaByIdQueryResult> Handle(GetSocialMediaByIdQuery request, CancellationToken cancellationToken)
+		{
+			var values = await _repository.GetByIdAsync(request.Id);
+
+			return new GetSocialMediaByIdQueryResult	
+			{
+				SocialMediaId = values.SocialMediaId,
+				Name = values.Name,
+				Icon = values.Icon,
+				Url = values.Url,
+			};
+		}
+	}
+}
---
+++ Core/CarBook.Application/Features/Mediator/Handlers/SocialMediaHandlers/UpdateSocialMediaCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Application.Features.Mediator.Commands.SocialMediaCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.SocialMediaHandlers
+{
+	public class UpdateSocialMediaCommandHandler : IRequestHandler<UpdateSocialMediaCommand>
+	{
+		private readonly IRepository<SocialMedia> _repository;
+
+		public UpdateSocialMediaCommandHandler(IRepository<SocialMedia> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task Handle(UpdateSocialMediaCommand request, CancellationToken cancellationToken)
+		{
+			var values = await _repository.GetByIdAsync(request.SocialMediaId);
+
+			values.Name = request.Name;
+			values.Icon = request.Icon;
+			values.Url = request.Url;
+
+			await _repository.UpdateAsync(values);
+		}
+	}
+}
