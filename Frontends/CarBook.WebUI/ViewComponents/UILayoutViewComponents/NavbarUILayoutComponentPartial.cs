20a8914828612c5333515aec0cd27aa0a6e3c36b
ssahin61 <umitsahinerdogan@gmail.com>
2025-01-04 18:55:42 +0300

    added about view components

---
+++ Frontends/CarBook.WebUI/ViewComponents/UILayoutViewComponents/NavbarUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.UILayoutViewComponents
+{
+	public class NavbarUILayoutComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
