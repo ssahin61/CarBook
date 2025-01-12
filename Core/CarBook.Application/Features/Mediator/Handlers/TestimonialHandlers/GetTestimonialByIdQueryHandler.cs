60749656d4acf1c5fe0cba2d3b2e42e8034de96c
ssahin61 <umitsahinerdogan@gmail.com>
2025-01-12 14:59:46 +0300

    added testimonial entity with mediator

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/TestimonialHandlers/GetTestimonialByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.Application.Features.Mediator.Queries.TestimonialQueries;
+using CarBook.Application.Features.Mediator.Results.TestimonialResults;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+
+namespace CarBook.Application.Features.Mediator.Handlers.TestimonialHandlers
+{
+	public class GetTestimonialByIdQueryHandler : IRequestHandler<GetTestimonialQuery, List<GetTestimonialQueryResult>>
+	{
+		private readonly IRepository<Testimonial> _repository;
+
+		public GetTestimonialByIdQueryHandler(IRepository<Testimonial> repository)
+		{
+			_repository = repository;
+		}
+
+		public async Task<List<GetTestimonialQueryResult>> Handle(GetTestimonialQuery request, CancellationToken cancellationToken)
+		{
+			var values = await _repository.GetAllAsync();
+
+			return values.Select(x => new GetTestimonialQueryResult
+			{
+				TestiMonialId = x.TestiMonialId,
+				Name = x.Name,
+				Title = x.Title,
+				ImageUrl = x.ImageUrl,
+			}).ToList();
+		}
+	}
+}
