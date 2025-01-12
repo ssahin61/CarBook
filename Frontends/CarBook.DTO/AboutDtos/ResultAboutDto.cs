c59ed7ffc88473ef09ce3f0b3cf6b06c6e806cbe
ssahin61 <umitsahinerdogan@gmail.com>
2025-01-12 16:30:03 +0300

    added testimonial view component and update about view component

--- Frontends/CarBook.DTO/AboutDtos/ResultAboutDto.cs
+++ Frontends/CarBook.DTO/AboutDtos/ResultAboutDto.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarBook.DTO.AboutDtos
+﻿namespace CarBook.DTO.AboutDtos
 {
 	public class ResultAboutDto
 	{
-		public int aboutId { get; set; }
-		public string title { get; set; }
-		public string description { get; set; }
-		public string imageUrl { get; set; }
-
+		public int AboutId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string ImageUrl { get; set; }
 	}
 }
---
+++ Frontends/CarBook.DTO/TestimonialDtos/ResultTestimonialDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarBook.DTO.TestimonialDtos
+{
+	public class ResultTestimonialDto
+	{
+		public int TestiMonialId { get; set; }
+		public string Name { get; set; }
+		public string Title { get; set; }
+		public string Comment { get; set; }
+		public string ImageUrl { get; set; }
+	}
+}
