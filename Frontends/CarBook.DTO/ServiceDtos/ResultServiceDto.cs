56f1e5b23f00664131a86d54937fe61e44edcd75
ssahin61 <umitsahinerdogan@gmail.com>
2025-01-18 21:16:25 +0300

    created service controller wtih index view

---
+++ Frontends/CarBook.DTO/ServiceDtos/ResultServiceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace CarBook.DTO.ServiceDtos
+{
+	public class ResultServiceDto
+	{
+		public int ServiceId { get; set; }
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public string IconUrl { get; set; }
+	}
+}
