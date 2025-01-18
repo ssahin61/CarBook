0cff620142cbc5e3f216e4409d24a4508dd73d6d
ssahin61 <umitsahinerdogan@gmail.com>
2025-01-18 21:17:27 +0300

    created car controller with index view

---
+++ Frontends/CarBook.WebUI/Controllers/CarController.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.DTO.CarDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.Controllers
+{
+	public class CarController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public CarController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:44345/api/Cars/GetCarWithBrand");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCarDto>>(jsonData);
+				return View(values);
+			}
+
+			return View();
+		}
+	}
+}
