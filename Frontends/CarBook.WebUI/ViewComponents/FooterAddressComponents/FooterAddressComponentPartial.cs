1eb983a7f22fae99b71a5bbd5587c15a61cfc1da
ssahin61 <umitsahinerdogan@gmail.com>
2025-01-19 13:58:21 +0300

    create contact controller with index view

---
+++ Frontends/CarBook.WebUI/ViewComponents/FooterAddressComponents/FooterAddressComponentPartial.cs
@@ -0,0 +1,31 @@
+﻿using CarBook.DTO.FooterAddressDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CarBook.WebUI.ViewComponents.FooterAddressComponents
+{
+	public class FooterAddressComponentPartial : ViewComponent
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public FooterAddressComponentPartial(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:44345/api/FooterAddresses");
+
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultFooterAddressDto>>(jsonData);
+				return View(values);
+			}
+
+			return View();
+		}
+	}
+}
