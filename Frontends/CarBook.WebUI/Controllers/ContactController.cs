f05c1e6c2d85c9428dda75c5c2284105e793671d
ssahin61 <umitsahinerdogan@gmail.com>
2025-01-19 20:07:49 +0300

    added sending contact message feature

--- Frontends/CarBook.WebUI/Controllers/ContactController.cs
+++ Frontends/CarBook.WebUI/Controllers/ContactController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CarBook.DTO.ContactDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
 
 namespace CarBook.WebUI.Controllers
 {
@@ -10,9 +13,24 @@ namespace CarBook.WebUI.Controllers
 		{
 			_httpClientFactory = httpClientFactory;
 		}
-
+		[HttpGet]
 		public IActionResult Index()
 		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Index(CreateContactDto createContactDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			createContactDto.SendDate = DateTime.UtcNow;
+			var jsonData = JsonConvert.SerializeObject(createContactDto);
+			StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:44345/api/Contacts", content);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index", "Contact");
+			}
 			return View();
 		}
 	}
